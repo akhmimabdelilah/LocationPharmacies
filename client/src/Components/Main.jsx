40a43ceb88d35c033583da9de9d718db563d4d38
Mohamed Essabir <mohamedessabir20@gmail.com>
2023-04-08 01:24:04 +0100

    useReducer hook

--- client/src/Components/Main.jsx
+++ client/src/Components/Main.jsx
@@ -1,4 +1,4 @@
-import {React, useEffect, useState} from 'react';
+import {React, useEffect, useState, useReducer} from 'react';
 import { Formik, Form } from 'formik';
 import Select from 'react-select'
 import Button from 'react-bootstrap/Button';
@@ -14,50 +14,83 @@ const defaultCity = { value: "city", label: "Select City" };
 const defaultZone = { value: "zone", label: "Select City" };
 const defaultGarde = { value: "garde", label: "Select Garde" };
 
+const initialState = {
+    cities: null,
+    city: null,
+    zones: null,
+    zone: null,
+    garde: null
+  };
+
+function reducer(state, action) {
+    switch (action.type) {
+      case 'SET_CITIES':
+        return {...state, cities: action.payload};
+      case 'SET_CITY':
+        return {...state, city: action.payload};
+      case 'SET_ZONES':
+        return {...state, zones: action.payload};
+      case 'SET_ZONE':
+        return {...state, zone: action.payload};
+      case 'SET_GARDE':
+        return {...state, garde: action.payload};
+      default:
+        throw new Error(`Unsupported action type: ${action.type}`);
+    }
+}
+
 
 const Main = () => {
-    const [cities, setCities] = useState(null);
-    const [city, setCity] = useState(null);
-    const [zones, setZones] = useState(null);
-    const [zone, setZone] = useState(null);
-    const [garde, setGarde] = useState(null);
+    // const [cities, setCities] = useState(null);
+    // const [city, setCity] = useState(null);
+    // const [zones, setZones] = useState(null);
+    // const [zone, setZone] = useState(null);
+    // const [garde, setGarde] = useState(null);
+
+    const [state, dispatch] = useReducer(reducer, initialState);
     const [pharmacies, setPharmacies] = useState(null);
 
     useEffect(() => {
-        fetch('http://127.0.0.1:5050/api/cities')
+        fetch('http://127.0.0.1:9000/api/cities')
             .then(response => response.json())
             .then(data => {
                 const options = data.map(item => ({ value: item.id, label: item.name }));
-                setCities(options);
+                // setCities(options);
+                dispatch({type: 'SET_CITIES', payload: options});
             })
             .catch(error => console.error(error));
 
-        fetch('http://127.0.0.1:5050/api/zones')
+        fetch('http://127.0.0.1:9000/api/zones')
             .then(response => response.json())
             .then(data => {
                 const options = data.map(item => ({ value: item.id, label: item.name }));
-                setZones(options);
+                // setZones(options);
+                dispatch({type: 'SET_ZONES', payload: options});
             })
             .catch(error => console.error(error));
     }, []);
 
 
-    const isCity = !city;
-    const isZone = !zone;
+    const isCity = !state.city;
+    const isZone = !state.zone;
 
     const handleCityChange = data => {
-        setCity(data);
-        setZone(null);
+        // setCity(data);
+        // setZone(null);
+        dispatch({type: 'SET_CITY', payload: data});
+        dispatch({type: 'SET_ZONE', payload: null});
     };
 
     const handleZoneChange = data => {
-        setZone(data);
-        setGarde(null);
+        // setZone(data);
+        // setGarde(null);
+        dispatch({type: 'SET_ZONE', payload: data});
+        dispatch({type: 'SET_GARDE', payload: null});
     }
 
     const handleGardeChange = data => {
-        setGarde(data);
-        // get data from database
+        // setGarde(data);
+        dispatch({type: 'SET_GARDE', payload: data});
     }
 
     return(
@@ -65,17 +98,17 @@ const Main = () => {
             <div className='d-flex justify-content-center'>
                 <div className='mx-3 flex-grow-1'>
                     <Select
-                        options={cities}
+                        options={state.cities}
                         defaultValue={defaultCity}
-                        value={city}
+                        value={state.city}
                         onChange={handleCityChange}
                     />
                 </div>
                 <div className='mx-3 flex-grow-1'>
                     <Select 
-                        options={zones} 
+                        options={state.zones} 
                         defaultValue={defaultZone}
-                        value={zone}
+                        value={state.zone}
                         onChange={handleZoneChange}
                         isDisabled={isCity}
                     />
@@ -84,7 +117,7 @@ const Main = () => {
                     <Select 
                         options={gardeData} 
                         defaultValue={defaultGarde}
-                        value={garde}
+                        value={state.garde}
                         onChange={handleGardeChange}
                         isDisabled={isZone}
                     />
